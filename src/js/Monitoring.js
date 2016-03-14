d36af48525eaa41769ab1fe98687c11efc5b7a29
mgarcia <miguelglafuente@gmail.com>
2016-03-14 12:06:21 +0100

    New icon to slide

--- src/js/Monitoring.js
+++ src/js/Monitoring.js
@@ -318,7 +318,7 @@ var Monitoring = (function () {
             }
         }.bind(this));
 
-        $(".burgercontainer").click(function(x) {
+        $(".slidecontainer").click(function(x) {
             // var elem = $(x.target);
             // var closing = elem.text() === "^";
             var closing = this.variables.closed.get() === "true";
@@ -326,9 +326,11 @@ var Monitoring = (function () {
             this.variables.closed.set("" + closing);
             if (closing) {
                 $(".navbar").collapse('hide');
+                $(".slidecontainer").removeClass("open").addClass("closed");
                 // elem.text('v');
             } else {
                 $(".navbar").collapse('show');
+                $(".slidecontainer").removeClass("closed").addClass("open");
                 // elem.text('^');
             }
             return false;
@@ -476,6 +478,9 @@ var Monitoring = (function () {
 
         if (this.variables.closed.get() === "true") {
             $(".navbar").collapse('hide');
+            $(".slidecontainer").removeClass("open").addClass("closed");
+        } else {
+            $(".slidecontainer").removeClass("closed").addClass("open");
         }
 
         var sort = this.variables.sort.get();
